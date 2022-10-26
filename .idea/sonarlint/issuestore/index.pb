e5ced827f088e227714f5b145142ff74b12466cd
Diogo Malfatti <diimalfatti@gmail.com>
2022-10-26 11:07:18 -0300

    Implementing possible moves of Rook

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,5 @@
 
 =
 chessGame.iml,9\6\964361e036afddee0b34f0622e8d7719fed1fed9
-K
-src/boardgame/Position.java,6\1\61f04ddcd53c8e2006fef8201e5cd678001b0dc4
 D
 src/chess/Color.java,6\f\6f5048016a57bbf452eb3bb3965bb62dfb909033
--- src/application/Program.java
+++ src/application/Program.java
@@ -20,11 +20,11 @@ public class Program {
         UI.printBoard(chessMatch.getPieces());
 
         System.out.println("");
-        System.out.println("Source: ");
+        System.out.print("Source: ");
         ChessPosition source = UI.readChessPosition(sc);
 
         System.out.println("");
-        System.out.println("Target :");
+        System.out.print("Target :");
         ChessPosition target = UI.readChessPosition(sc);
 
         ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
